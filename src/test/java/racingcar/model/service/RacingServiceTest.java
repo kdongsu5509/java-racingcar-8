abb239db8caf0c382b679a69641b9438e59137b7
kdongsu5509 <kod66170@gmail.com>
2025-10-24 17:08:00 +0900

    test(B-2): add test logic to test random go logic
    랜덤으로 생성되는 숫자에 따라 자동차가 움직이는 로직을 테스트 하는 로직을 추가함

--- src/test/java/racingcar/model/service/RacingServiceTest.java
+++ src/test/java/racingcar/model/service/RacingServiceTest.java
@@ -22,4 +22,22 @@ class RacingServiceTest {
         List<Car> savedCars = carRepository.findAll();
         Assertions.assertEquals(3, savedCars.size());
     }
+
+    @Test
+    void doRacingAndReturnCurrentStatus_success() {
+        //given
+        List<String> testCars = List.of("car1", "car2");
+        CarRepository carRepository = new CarRepository();
+        RacingService racingService = new RacingService(carRepository);
+        racingService.setupRacingCars(testCars);
+
+        //when
+        racingService.race();
+
+        //then
+        List<Car> racedCars = carRepository.findAll();
+        for (Car car : racedCars) {
+            Assertions.assertTrue(car.getPosition() >= 0 && car.getPosition() <= 1);
+        }
+    }
 }
