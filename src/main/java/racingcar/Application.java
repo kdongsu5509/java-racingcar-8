d7d9d04f0a093f7112b73da37da8c9516cf9a7ae
kdongsu5509 <kod66170@gmail.com>
2025-10-25 22:15:09 +0900

    feat(C-3): complete main method to run properly
    전체 프로그램이 동작하도록 main 메소드를 완성함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.model.repository.CarRepository;
+import racingcar.model.service.RacingService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarRepository carRepository = new CarRepository();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        RacingService racingService = new RacingService(carRepository);
+        RacingCarController controller = new RacingCarController(inputView, outputView, racingService);
+
+        controller.start();
     }
 }
