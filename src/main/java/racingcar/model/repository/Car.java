e939d8432379061fd0d56a39a48550d789816c5f
kdongsu5509 <kod66170@gmail.com>
2025-10-26 17:13:22 +0900

    fix(B-1): change construct logic
    정상적인 입력이 아니면 자동차를 생성하지 않도록 코드를 수정함

--- src/main/java/racingcar/model/repository/Car.java
+++ src/main/java/racingcar/model/repository/Car.java
@@ -1,5 +1,8 @@
 package racingcar.model.repository;
 
+import racingcar.global.CustomException.CarException;
+import racingcar.global.ErrorMessage;
+
 public class Car {
 
     private final String name;
@@ -7,8 +10,30 @@ public class Car {
     private int position;
 
     public Car(String name) {
+        checkNameValidation(name);
         this.name = name;
         this.position = 0;
+    }
+
+    private void checkNameValidation(String name) {
+        isNotNull(name);
+        isNotOverLengthFive(name);
+    }
+
+    private static void isNotOverLengthFive(String name) {
+        if (name.length() > 5) {
+            throw new CarException(
+                    ErrorMessage.OVER_SIZE_FIVE.getMessage()
+            );
+        }
+    }
+
+    private void isNotNull(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new CarException(
+                    ErrorMessage.CANNOT_EMPTY.getMessage()
+            );
+        }
     }
 
     public String getName() {
